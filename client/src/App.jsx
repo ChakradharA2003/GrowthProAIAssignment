2b7a9ea7a080b6375331416422303d577ad9e4b8
ChakradharA2003 <chakradharchakri018@gmail.com>
2025-07-04 15:29:10 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -64,14 +64,18 @@ function App() {
                 placeholder="Business Name"
                 className="w-full px-4 py-2 bg-green-50 border border-green-200 rounded-lg focus:ring-2 focus:ring-green-300 focus:border-green-500 text-gray-800 placeholder-gray-600 text-lg transition-all duration-300 animate-slide-up md:text-xl text-lg"
                 value={name}
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => {setName(e.target.value)
+                  setError('');  
+                }}
               />
               <input
                 type="text"
                 placeholder="Location"
                 className="w-full px-4 py-2 bg-green-50 border border-green-200 rounded-lg focus:ring-2 focus:ring-green-300 focus:border-green-500 text-gray-800 placeholder-gray-600 text-lg transition-all duration-300 animate-slide-up md:text-xl text-lg"
                 value={location}
-                onChange={(e) => setLocation(e.target.value)}
+                onChange={(e) => {setLocation(e.target.value),
+                  setError('');
+                }}
               />
               {error && <p className="text-red-500 text-sm font-medium bg-red-100/80 rounded-lg p-2 text-center animate-fade-in">{error}</p>}
               <button
