c0e58c1426de4f39fe94a0189e171c0235bd7f63
ChakradharA2003 <144037702+ChakradharA2003@users.noreply.github.com>
2025-07-05 08:34:49 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -67,6 +67,7 @@ function App() {
       <input
         type="text"
         placeholder="Business Name"
+        required
         className="w-full px-4 py-2 bg-green-50 border border-green-200 rounded-lg focus:ring-2 focus:ring-green-300 focus:border-green-500 text-gray-800 placeholder-gray-600 text-lg transition-all duration-300 md:text-xl"
         value={name}
         onChange={handleInputChange(setName)}
@@ -74,6 +75,7 @@ function App() {
       <input
         type="text"
         placeholder="Location"
+        required
         className="w-full px-4 py-2 bg-green-50 border border-green-200 rounded-lg focus:ring-2 focus:ring-green-300 focus:border-green-500 text-gray-800 placeholder-gray-600 text-lg transition-all duration-300 md:text-xl"
         value={location}
         onChange={handleInputChange(setLocation)}
